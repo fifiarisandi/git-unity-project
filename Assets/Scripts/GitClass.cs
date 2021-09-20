14a4bfb88c468730caca1fd78e91a07abcbdf59d
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2021-09-20 19:46:57 +0300

    add git script

---
+++ Assets/Scripts/GitClass.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitClass : MonoBehaviour
+{
+	string myName = "Git";
+	int myPower = 9001;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		print("Hello! My name is " + myName);
+		
+        if (myPower > 9000)
+		{
+			print("It's over 9000!!!");
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
