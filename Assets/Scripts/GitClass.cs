65abcf13e7fa79ec0942793077b356cc60dc6bd3
merttuncel <82678965+merttuncel3@users.noreply.github.com>
2021-09-22 19:16:22 +0300

    variables changed for extra points

--- Assets/Scripts/GitClass.cs
+++ Assets/Scripts/GitClass.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class GitClass : MonoBehaviour
 {
-	string myName = "Git";
-	int myPower = 9001;
+	string myName = "MertProject";
+	int myPower = 1000;
 	
     // Start is called before the first frame update
     void Start()
