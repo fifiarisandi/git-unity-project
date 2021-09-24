14b339fdb8b75749c778c9d0e76ac516c5018c1e
nathan.amorrich <namorric@tlu.ee>
2021-09-24 16:39:59 +0300

    Challenge. GitClass.cs  modifications.
    added "myLife" variable.
    added an "if" statement.
    modified first "if" condition.
    modified the printed messages.

--- Assets/Scripts/GitClass.cs
+++ Assets/Scripts/GitClass.cs
@@ -4,25 +4,31 @@ using UnityEngine;
 
 public class GitClass : MonoBehaviour
 {
-	string myName = "MertProject";
-	int myPower = 1000;
+	string myName = "Nathan";
+	int myPower = 9001;
+    int myLife = 10;
 	
     // Start is called before the first frame update
     void Start()
     {
-		print("Who is aswesome?:" + myName);
+		print("Hello there, from " + myName);
 		
-        if (myPower < 9000)
+        if (myPower > 9000)
 		{
-			print("Power overwhelming!");
+			print("It's over 9000 !");
     	}
     
-		print("Hola, ladies and gentlemen. I am " + myName);
+		print("Bonjour, ladies and gentlemen. I am " + myName);
 		
         if (myPower > 1)
 		{
-			print("And I will play for you a spanish guitar!");
+			print("And I will play for you the baguette.");
 		}
+
+        if (myLife < 1)
+        {
+            print("oof");
+        }
     }
 
     // Update is called once per frame
